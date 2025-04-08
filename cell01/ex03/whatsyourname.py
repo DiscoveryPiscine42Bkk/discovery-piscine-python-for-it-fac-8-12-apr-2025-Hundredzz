ceca513db20ff08e978863e8d4b977b413056c63
Hundredz <144457401+Hundredzz@users.noreply.github.com>
2025-04-08 20:18:19 +0700

    update

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,6 +1,6 @@
 print("Hey, what's your first name? :", end = " ")
-first_name = input()
+first_name = input().strip()
 print("And your last name? :", end = " ")
-last_name = input()
+last_name = input().strip()
 whole_name = first_name + " " + last_name
 print(f"Well, pleased to meet you, {whole_name}.")
