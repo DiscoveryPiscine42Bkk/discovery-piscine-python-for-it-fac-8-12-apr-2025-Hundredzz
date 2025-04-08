bdc330317ef535b4a680c342e277919e29d4e9cf
Hundredz <144457401+Hundredzz@users.noreply.github.com>
2025-04-08 20:12:15 +0700

    cell1.0-1.3

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Asitara"
+last_name = "Phumdokmai"
+print(f"{first_name} {last_name}")
---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,6 @@
+print("Hey, what's your first name? :", end = " ")
+first_name = input()
+print("And your last name? :", end = " ")
+last_name = input()
+whole_name = first_name + " " + last_name
+print(f"Well, pleased to meet you, {whole_name}.")
